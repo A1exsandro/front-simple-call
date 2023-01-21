fed6b53128f0f2a011fc9a79ff140b8c279dc545
A1exsandro <alexsandro.acp@gmail.com>
2023-01-21 19:01:39 -0400

    add styles student

---
+++ src/components/Students/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+ 
+export const StudentContainer = styled.div`
+  width: 100%;
+  max-width: 400px;
+  margin: 30px;
+  background: #FFF;
+  border-radius: 4px;
+  padding: 20px;
+  
+	cursor: pointer;
+
+  transition: height 0.2s ease; 
+`
+ 
