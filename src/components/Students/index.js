1df22735aa7702151c4af6cc245e2a9cbd5cb78d
A1exsandro <alexsandro.acp@gmail.com>
2023-01-22 14:56:06 -0400

    add axios

--- src/components/Students/index.js
+++ src/components/Students/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { 
   StudentContainer,
   StudentAvatar,
@@ -9,19 +11,31 @@ import {
   StudentName
 } from './styles'
 
-function Student() {
-  return (  
-    <StudentContainer>
-      <StudentAvatar>AV</StudentAvatar>
-      <StudentDescription>
-        <StudentPresence>
-          <StudentPresent>Presenças: 64</StudentPresent>
-          <StudentAbsent>Faltas: 01</StudentAbsent>
-        </StudentPresence>
-        <StudentName>Student</StudentName> 
-      </StudentDescription>
-      <StudentStar>*****</StudentStar>
-    </StudentContainer>
+const baseURL = 'http://localhost:3000/students'
+
+function Student() { 
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setStudents(response.data)
+    })
+  },[students])
+
+  return ( 
+    students.map((student, index) => (
+      <StudentContainer key={index}>
+        <StudentAvatar>{index}</StudentAvatar>
+        <StudentDescription>
+          <StudentPresence>
+            <StudentPresent onClick={() => (alert('o Aluno está Presente'))}>Presenças: {student.present}</StudentPresent>
+            <StudentAbsent onClick={() => (alert('o Aluno Faltou'))}>Faltas: {student.present}</StudentAbsent>
+          </StudentPresence>
+          <StudentName>{student.name}</StudentName> 
+        </StudentDescription>
+        <StudentStar>*****</StudentStar>
+      </StudentContainer>
+    )) 
   )
 }
 
