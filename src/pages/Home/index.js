c0928bfa2eb5e990872a28bef6249fe28b843594
A1exsandro <alexsandro.acp@gmail.com>
2023-01-23 15:51:14 -0400

    add page Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { Content } from './styles'
+
+function Home(){
+	return(
+		<Content>
+			Never Stop Trying 
+		</Content>
+	)
+}
+
+export default Home
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 400px;
+  margin: 30px;
+  background: #FFF;
+  border-radius: 4px;
+  padding: 1rem;
+`
