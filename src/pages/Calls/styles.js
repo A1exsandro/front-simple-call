c4bbd31fc13b73cae04037498c1c581931571a29
A1exsandro <alexsandro.acp@gmail.com>
2023-01-23 16:50:53 -0400

    add styles in calls

---
+++ src/pages/Calls/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components" 
+
+export const CallsContainer = styled.div`
+
+`
+export const CallsTitle = styled.div`
+
+`
+export const CallDaily = styled.div`
+  margin: 1rem;
+`
+export const CallDate = styled.div`
+
+`
+export const CallStudents = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const CallName = styled.div`
+
+`
+export const CallPresent = styled.div`
+
+`
