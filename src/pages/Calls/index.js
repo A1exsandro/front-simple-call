4c718568aacbcddcf6715bb7108c5bf8211c4e4c
A1exsandro <alexsandro.acp@gmail.com>
2023-01-24 22:24:53 -0400

    refactor

--- src/pages/Calls/index.js
+++ src/pages/Calls/index.js
@@ -14,32 +14,32 @@ import {
 const baseURL = 'http://localhost:3000/calls'
 
 function Calls() { 
-  const [calls, setcalls] = useState([])
+  const [calls, setCalls] = useState([])
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
-      setcalls(response.data)
+      setCalls(response.data)
     })
   },[])
  
   return(
     <CallsContainer>
       <CallsTitle>Calls</CallsTitle>
-        {
-          calls.map((call, index) => ( 
-            <CallDaily key={index}>
-              <CallDate>{ call.date }</CallDate>
-              {
-                  call.students.map((student, index) => (
-                  <CallStudents key={index}> 
-                    <CallName>{student.name}</CallName>
-                    <CallPresent>{student.present.toString()}</CallPresent>
-                  </CallStudents>
-                ))
-              } 
-            </CallDaily>
-          ))
-        }  
+      {
+        calls.map((call, index) => ( 
+          <CallDaily key={index}>
+            <CallDate>{ call.date }</CallDate>
+            {
+                call.students.map((student, index) => (
+                <CallStudents key={index}> 
+                  <CallName>{student.name}</CallName>
+                  <CallPresent>{student.present.toString()}</CallPresent>
+                </CallStudents>
+              ))
+            } 
+          </CallDaily>
+        ))
+      }   
     </CallsContainer>
      
   )
