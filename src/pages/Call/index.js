5721e05b40d18a7f908d813989d459a532026b8b
A1exsandro <alexsandro.acp@gmail.com>
2023-01-24 22:26:32 -0400

    axios in call

--- src/pages/Call/index.js
+++ src/pages/Call/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import axios from 'axios'
 import Student from '../../components/Students'
 
 import { 
@@ -8,16 +10,34 @@ import {
 	CallResume
 } from './styles'
 
+const baseURL = 'http://localhost:3000'
+
 function Call() {
+  const [data, setData] = useState([])
+
+  const childToParent = (childdata) => {
+    setData(childdata);
+  } 
+
+	function createCall() {
+		axios.post(`${baseURL}/calls`, { 
+      date:  '2023-01-01',
+		  students: data 
+		}) 
+  }
+
 	return (
 		<CallContainer>
 			<CallTitle>Call</CallTitle> 
 			<CallDate type="date"></CallDate>
-			<Student />  
+
+			<Student childToParent={childToParent}/> 
+
 			<CallResume>
 				Total de Alunos: 45 - Total de Faltas: 5
 			</CallResume>
 			<CallSubmit onClick={() => (alert('Chamada Realizada com Sucesso!!!'))}>Enviar</CallSubmit>
+			<button onClick={createCall}>Criar</button>
 		</CallContainer>
 	)
 }
