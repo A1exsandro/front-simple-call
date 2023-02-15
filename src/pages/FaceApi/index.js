ad7e04bf212e1222d85140f8072671aa66888046
A1exsandro <alexsandro.acp@gmail.com>
2023-02-15 13:36:44 -0400

    add page face api

---
+++ src/pages/FaceApi/index.js
@@ -0,0 +1,111 @@
+import * as faceapi from 'face-api.js'
+import React, { useEffect, useRef, useState } from 'react'
+
+const FaceApi = () => {
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [captureVideo, setCaptureVideo] = useState(false);
+
+  const videoRef = useRef();
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const canvasRef = useRef();
+
+  // useEffect(() => {
+  //   const loadModels = async () => {
+  //     const MODEL_URL = './models-face-api';
+
+  //     Promise.all([
+  //       faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+  //     ]).then(setModelsLoaded(true));
+  //   }
+  //   loadModels();
+  // }, []);
+
+  const cam = document.getElementById('cam')
+  
+  const startVideo = () => {
+    setCaptureVideo(true);
+    navigator.mediaDevices.enumerateDevices()
+  .then(devices => {
+    if (Array.isArray(devices)) {
+      devices.forEach(device => {
+        if (device.kind === 'videoinput') {
+          navigator.getUserMedia(
+            { video: {
+                deviceId: device.deviceId
+            }},
+            stream => cam.srcObject = stream,
+            error => console.error(error)
+          )
+        }
+      })
+    }
+  })
+  }
+
+  const handleVideoOnPlay = () => {
+    setInterval(async () => {
+      if (canvasRef && canvasRef.current) {
+        canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+        const displaySize = {
+          width: videoWidth,
+          height: videoHeight
+        }
+
+        faceapi.matchDimensions(canvasRef.current, displaySize);
+
+        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+        canvasRef && canvasRef.current && canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+        canvasRef && canvasRef.current && faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+        canvasRef && canvasRef.current && faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+        canvasRef && canvasRef.current && faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+      }
+    }, 100)
+  }
+
+  const closeWebcam = () => {
+    videoRef.current.pause();
+    videoRef.current.srcObject.getTracks()[0].stop();
+    setCaptureVideo(false);
+  }
+
+  return (
+    <div>
+      <div style={{ textAlign: 'center', padding: '10px' }}>
+        {
+          captureVideo && modelsLoaded ?
+            <button onClick={closeWebcam} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
+              Close Webcam
+            </button>
+            :
+            <button onClick={startVideo} style={{ cursor: 'pointer', backgroundColor: 'green', color: 'white', padding: '15px', fontSize: '25px', border: 'none', borderRadius: '10px' }}>
+              Open Webcam
+            </button>
+        }
+      </div>
+      {
+        captureVideo ?
+          modelsLoaded ?
+            <div>
+              <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+                <video ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} style={{ borderRadius: '10px' }} />
+                <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+              </div>
+            </div>
+            :
+            <div>loading...</div>
+          :
+          <>
+      </>
+      }
+    </div>
+  )
+}
+ 
+export default FaceApi
