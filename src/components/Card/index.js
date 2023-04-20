646e22dfd3af9684951824462d144550bb0ae88c
A1exsandro <alexsandro.acp@gmail.com>
2023-04-20 11:14:01 -0400

    feat: add audio in card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react" 
+import React, { useRef, useState } from "react" 
 
 import './styles.css'
 
 const Card = ({ front, back, srcImg}) => {
 	const [flipped, setFlipped] = useState(false)
 	const [hovered, setHovered] = useState(false)
+  const audioRef = useRef(null)
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play()
+    }
+  }
 
 	const handleClick = () => {
 		setFlipped(!flipped)
+
+    if (flipped == false) {
+      playAudio()
+    }
 	}
 
 	return (
@@ -20,7 +31,12 @@ const Card = ({ front, back, srcImg}) => {
 			<div className="card-face card-front">
 				<img src={srcImg} alt={front} />
 			</div>
-			<div className="card-face card-back">{back}</div>
+        <div className="card-face card-back">
+          {back}
+          <audio ref={audioRef}>
+            <source src="https://firebasestorage.googleapis.com/v0/b/letmeask-333417.appspot.com/o/guava.mp3?alt=media&token=9954a02d-e0cd-4010-b78d-3f91809b9391" />
+          </audio>
+        </div>
 		</div>
 	)
 }
