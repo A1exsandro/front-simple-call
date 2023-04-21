ace30a10a67af018ccaf481f8d87bc38c8487481
A1exsandro <alexsandro.acp@gmail.com>
2023-04-21 17:03:54 -0400

    feat: state openCards

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ A app simple of call
 
 ## Face Api
 * npm install face-api.js
+
+## Some dependencies
+* npm install firebase
--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,12 +1,25 @@
-import React from "react" 
+import React, { useState } from "react" 
 import Card from "../Card";
 import { CardListContainer } from "./styles";
 
 const CardList = ({ cards }) => {
+  const [openCards, setOpenCards] = useState(0)
+
+  const handleClick = () => {
+    setOpenCards(openCards + 1)
+	}
+
   return (
     <CardListContainer>
       {cards.map((card) => (
-        <Card key={card.id} srcImg={card.srcImg} front={card.front} back={card.back} />
+        <Card 
+          key={card.id} 
+          srcImg={card.srcImg} 
+          front={card.front} 
+          back={card.back} 
+          openCards={openCards}
+          onClick={handleClick} 
+        />
       ))}
     </CardListContainer>
   );
