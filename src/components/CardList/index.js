a4a809f1a070606fd451039b4b62aea4e378d97d
A1exsandro <alexsandro.acp@gmail.com>
2023-04-23 12:32:43 -0400

    feat: resolver promises

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,20 +1,49 @@
 // import { pairsOfCards } from "../../Utils/cards_2";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMemory } from "../../contexts/MemoryContext";
+import { audioPromises, promises } from "../../Utils/Cards_2";
 import Card from "../Card";
 import { CardListContainer } from "./styles";
 
 const CardList = () => { 
-  const { cards, startGame } = useMemory()
+  // const { cards, startGame, setCards } = useMemory()
+  const [cards, setCards] = useState([])
+  const [sounds, setSounds] = useState([])
 
   useEffect(() => {
-    startGame()
-  }, [])
-  console.log(cards)
+    Promise.all(promises)
+      .then((urls) => setCards(urls))
+
+    Promise.all(audioPromises)
+      .then((audios) => setSounds(audios))
+  },[])
+
+  const uniqueCards = cards.map((img, bothId) => ({
+    bothId,
+    img,
+  }))
+
+  const uniqueSounds = sounds.map((sound, bothId) => ({
+    bothId,
+    sound,
+  }))
+  
+  const pairsOfCards = [...uniqueCards, ...uniqueSounds].map(
+    (card, id) => ({
+    ...card, 
+    id
+  }))
+   console.log('mmmmmmmm', pairsOfCards)
+   console.log('aaasasasa', sounds)
+
+  // useEffect(() => {
+  //   startGame()
+  // }, [])
+  console.log('here in card list === cards', cards) 
 
   return (
     <CardListContainer>
-      {cards.map((card) => (
+      {pairsOfCards.map((card) => (
         <Card key={card.id} {...card}/>
       ))}
     </CardListContainer>
