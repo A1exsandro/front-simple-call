3b42049efed2536b5e24686845a403fdbfc591b1
A1exsandro <alexsandro.acp@gmail.com>
2023-04-25 16:20:58 -0400

    refactor: changes in get database

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,14 +1,39 @@
 // import { pairsOfCards } from "../../Utils/cards_2";
 import { useEffect, useState } from "react";
-import { useMemory } from "../../contexts/MemoryContext";
-import { audioPromises, promises } from "../../Utils/Cards_2";
+import { useMemory } from "../../contexts/MemoryContext"; 
 import Card from "../Card";
 import { CardListContainer } from "./styles";
+
+import { getStorage } from '../../services/Firebase'
+import { ref, getDownloadURL } from "firebase/storage" 
+
+const storage = getStorage() 
+
+const data = [
+  'butterfly',
+  'avocado',
+  'banana',
+  'bathroom',
+  'breakfast',
+  'closed',
+  'dance',
+  'eat',
+  'fear'
+] 
+console.log('here in cards')
 
 const CardList = () => { 
   // const { cards, startGame, setCards } = useMemory()
   const [cards, setCards] = useState([])
   const [sounds, setSounds] = useState([])
+
+  const promises = data.map((dt) => (
+    getDownloadURL(ref(storage, `images/${dt}.jpeg`))
+  ))
+  
+  const audioPromises = data.map((dt) => (
+    getDownloadURL(ref(storage, `audio/${dt}.mp3`))
+  ))
 
   useEffect(() => {
     Promise.all(promises)
